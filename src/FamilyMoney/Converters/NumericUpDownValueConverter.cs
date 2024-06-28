fc03f54b77ff7b1dd2d4b7a936ca2c9256c168ca
daniilns <danil@live.ru>
2024-06-28 18:22:46 +0300

    fix empty sum field exception

---
+++ src/FamilyMoney/Converters/NumericUpDownValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace FamilyMoney.Converters;
+
+public class NumericUpDownValueConverter: IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return default(decimal);
+        return (decimal?)((IConvertible)value).ToDecimal(culture);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+        {
+            if (targetType == typeof(decimal))
+                return 0m;
+
+            throw new ArgumentNullException($"Unsupported type: {targetType.FullName}");
+        }
+
+        if (targetType == typeof(decimal))
+            return ((IConvertible)value).ToDecimal(culture);
+
+        throw new ArgumentNullException($"Unsupported type: {targetType.FullName}");
+    }
+}
