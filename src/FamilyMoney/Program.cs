919aa5110fe53a0d0a2f4991059846f12482eb25
daniilns <danil@live.ru>
2026-06-07 11:17:48 +0300

    avalonia 12

--- src/FamilyMoney/Program.cs
+++ src/FamilyMoney/Program.cs
@@ -16,6 +16,9 @@ internal sealed class Program
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
+#if DEBUG
+            .WithDeveloperTools()
+#endif
             .WithInterFont()
             .LogToTrace();
 }
